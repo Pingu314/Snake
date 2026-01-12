7850323b39511343e1981616ee4f576f982561ea
Pingu314 <pingu314159@gmail.com>
2026-01-12 20:22:18 +0100

    v1.1: add module audio,animations,input skeletons

---
+++ v1.1/Audio.py
@@ -0,0 +1,13 @@
+# v1.1 Audio system
+# Skeleton - no behaviour change
+
+MUTED = False
+def play(sound_name):
+
+
+    pass
+
+def toggle_mute():
+    global MUTED
+    MUTED = not MUTED
+    return MUTED
